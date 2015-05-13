e0fa113d1de4abc8b6a58c75bdc06613e347b4f4
Alejo Ribes <alejoribes@gmail.com>
2015-05-13 12:00:02 +0200

    First Commit, new Exercise, Adress Book, view all Contacts

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Contact.create(name:'Pepito', adress:'AVD Abuela en bicicleta, 4', number_phone:'655223311', email: 'asdg@gmail.com')
+Contact.create(name:'Jose Emilio', adress:'Desengaño 21', number_phone:'65522221', email: 'turururur@gmail.com')
+Contact.create(name:'Francisco José Majete', adress:'Calle Fuencarral, 4', number_phone:'655223311', email: 'majete@gmail.com')
